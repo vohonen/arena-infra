f1745e5632b400797445a342fe62f031cc0a359a
josh-hills <joshtomhills@gmail.com>
2025-05-29 16:45:50 +0000

    added nginx setup script

--- proxy/setup_nginx.sh
+++ proxy/setup_nginx.sh
@@ -1,1 +1,9 @@
 sudo apt update && sudo apt install nginx-full git vim -y
+
+cp ./update.sh ~/
+cp ./nginx.conf /etc/nginx/nginx.conf
+mkdir /etc/nginx/streams-enabled
+touch /etc/nginx/streams-enabled/proxy.conf
+cd ~
+ln -s /etc/nginx/streams-enabled/proxy.conf .
+
